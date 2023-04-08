8bac6f4765951a9b783fe5b337e715ac9629923e
Sophia <sophia.oquias@gmail.com>
2023-04-08 15:14:40 +0800

    connected to the other nodes

--- index.js
+++ index.js
@@ -16,26 +16,82 @@ app.engine("hbs", exphbs.engine({
 app.use(express.static('public'));
 
 const mysql = require('mysql2');
-var con = mysql.createConnection({
-    host: '172.16.3.101',
-    port: '3306',
+var con0 = mysql.createConnection({
+    host: 'ccscloud3.dlsu.edu.ph',
+    port: '39000',
     user: 'dev',
     password: '12341234'
 });
 
-con.connect(function (err) {
+var con1 = mysql.createConnection({
+    host: 'ccscloud3.dlsu.edu.ph',
+    port: '39001',
+    user: 'dev',
+    password: '12341234'
+});
+
+var con2 = mysql.createConnection({
+    host: 'ccscloud3.dlsu.edu.ph',
+    port: '39002',
+    user: 'dev',
+    password: '12341234'
+});
+
+con0.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
 
-    console.log('connected as id ' + con.threadId);
+    console.log('connected as id ' + con0.threadId);
+});
+
+con1.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + con1.threadId);
+});
+
+con2.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + con2.threadId);
 });
 
 app.get('/', function (req, res) {
     //   res.sendFile(path.join(__dirname, '/index.html'));
-    var query = "SELECT * FROM imdb.movies LIMIT 10;"
-    con.query(query, function (error, results, fields) {
+    var query = "SELECT * FROM imdb.movies LIMIT 30;"
+    con0.query(query, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        // connected!
+
+        res.render('index', { tuple: results });
+    });
+});
+
+app.get('/1', function (req, res) {
+    //   res.sendFile(path.join(__dirname, '/index.html'));
+    var query = "SELECT * FROM imdb.movies LIMIT 30;"
+    con1.query(query, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        // connected!
+
+        res.render('index', { tuple: results });
+    });
+});
+
+app.get('/2', function (req, res) {
+    //   res.sendFile(path.join(__dirname, '/index.html'));
+    var query = "SELECT * FROM imdb.movies LIMIT 30;"
+    con2.query(query, function (error, results, fields) {
         if (error) throw error;
         console.log(results);
         // connected!
