03b9343fd4ec6f85bc51acf643c4045eef3b5931
Sophia <sophia.oquias@gmail.com>
2023-04-17 01:45:57 +0800

    asdfjk

--- index.js
+++ index.js
@@ -423,8 +423,22 @@ updateMovie = (pool, isolationLevel, id, name, year, rank) => {
                     }
                     newLog.status = COMMITTED;
                     log(historyPath, newLog);
-                    connection.execute("COMMIT;");
-                    return resolve(); 
+                    if(pool == pool1) {
+                        async function wait() {
+                            const sleep = ms => new Promise(r => setTimeout(r, ms));
+                            await sleep(10000) // await needs to be inside an async function
+                            // code after await and INSIDE THE FUNCTION is executed after the wait time
+                            // TODO: insert code to do after below
+                            console.log("After 10 seconds");
+                            connection.execute("COMMIT;");
+                            return resolve(); 
+                        }
+                        wait();
+                    }
+                    else {
+                        connection.execute("COMMIT;");
+                        return resolve(); 
+                    }
                 });
             });
             console.log("Connection released");
