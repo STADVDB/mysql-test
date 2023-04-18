728a3e5d3670bb96243947609145b4eadc5509b7
Sophia <sophia.oquias@gmail.com>
2023-04-18 20:33:01 +0800

    added delay

--- index.js
+++ index.js
@@ -472,8 +472,21 @@ updateMovie = (pool, isolationLevel, id, name, year, rank) => {
                     }
                     newLog.status = COMMITTED;
                     log(historyPath, newLog);
-                    connection.execute("COMMIT;");
-                    return resolve();
+
+                    console.log("Start of delay")
+
+                    async function wait() {
+                        const sleep = ms => new Promise(r => setTimeout(r, ms));
+                        await sleep(10000) // await needs to be inside an async function
+                        // code after await and INSIDE THE FUNCTION is executed after the wait time
+                        // TODO: insert code to do after below
+                        connection.execute("COMMIT;");
+                        console.log("After 10 seconds")
+                        return resolve();
+                    }
+
+                    wait(); 
+
                 });
             });
             console.log("Connection released");
